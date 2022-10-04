04b29d3d7849c2cc38223b47bad79ced9a5bf06a
Yurimancosu <yuri.mancosu@buonarroti.tn.it>
2022-10-04 11:28:16 +0200

    secondo commit

--- index.php
+++ index.php
@@ -9,10 +9,11 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <title></title>
     </head>
-    <body>
+    <body class="">
         <?php
         // put your code here
-        //ciao   
+        //ciao 
+        
         
         ?>
     </body>
